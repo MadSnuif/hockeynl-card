e12894505357be29ad3b712a41edb24ee63544ed
Max Laoh <max.laoh@outlook.com>
2024-04-01 20:36:09 +0200

    Border on entity fixed

--- dist/hockeynl-card.js
+++ dist/hockeynl-card.js
@@ -26,7 +26,7 @@ class HockeyNLCard extends HTMLElement {
           align-items: center;
           border-style: solid;
           border-width: var(--ha-card-border-width,1px);
-          border-radius: var(--ha-card-border-radius,12px);
+          border-radius: 12px; /*border-radius: var(--ha-card-border-radius,12px);*/
           padding: 5px 0px;
           border-color: var(--divider-color,#e0e0e0);
           margin: 5px; /* seperation between the entities */
